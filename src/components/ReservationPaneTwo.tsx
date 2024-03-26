f422c0c46ee0caac165f37bfde48dc5d6eef4e55
6633256621 <6633256621@student.chula.ac.th>
2024-03-26 22:49:18 +0700

    Y

--- src/components/ReservationPaneTwo.tsx
+++ src/components/ReservationPaneTwo.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { Date } from 'mongoose'
 
 import getReservations from '@/libs/getReservations'
 import { ReservationItem, ReservationJson } from '../../interface'
